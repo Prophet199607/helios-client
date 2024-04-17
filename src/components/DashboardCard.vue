e1ee164437c63a91c931130dafad8a5a6ba24af2
Pasindu Priyankara <pasinduxx@hotmail.com>
2024-04-17 16:36:06 +0530

    modified Dashboard Cards 2

--- src/components/DashboardCard.vue
+++ src/components/DashboardCard.vue
@@ -10,20 +10,6 @@ defineProps({
 </script>
 
 <template>
-<!--  <div class="flex gap-x-3 items-center shadow-md px-4 py-4">-->
-<!--    <div-->
-<!--        class="w-16 h-16 p-4 rounded-full flex justify-center items-center text-white"-->
-<!--        :class="bg_color"-->
-<!--    >-->
-<!--      <svg viewBox="0 0 482.085 482.085" width="60px" height="60px" fill="#fff">-->
-<!--        <g><use :xlink:href="`#${svg_icon}`"></use></g>-->
-<!--      </svg>-->
-<!--    </div>-->
-<!--    <div class="w-32">-->
-<!--      <div :class="title_class">{{ title }}</div>-->
-<!--      <div class="font-medium text-lg-c">{{ value }}</div>-->
-<!--    </div>-->
-<!--  </div>-->
   <div class="flex gap-x-3 items-center shadow-md px-4 py-4 rounded-2xl" :class="bg_color">
     <div class="w-32">
       <div class="font-bold text-xl">{{ value }}</div>
