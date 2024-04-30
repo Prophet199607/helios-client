d325e9b1d6afeca20483b1b0f8ed93102af3d310
Prophet199607 <pasinduxx@hotmail.com>
2024-04-30 13:09:19 +0530

    added login selenium test file 4

--- tests/login_test.py
+++ tests/login_test.py
@@ -26,7 +26,7 @@ password_input = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.CSS_SELECTOR, "input[id='password']"))
 )
 login_button = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))
+    EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit2']"))
 )
 
 # Enter your login credentials
