8cb4a49be074879967933dd9e7256a2a751adc78
Pasindu Priyankara <pasinduxx@hotmail.com>
2024-05-03 02:41:09 +0530

    modified Index.vue file 2

--- tests/login_test.py
+++ tests/login_test.py
@@ -18,10 +18,10 @@ username_input = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.CSS_SELECTOR, "input[id='username']"))
 )
 password_input = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, "input[id='password2']"))
+    EC.presence_of_element_located((By.CSS_SELECTOR, "input[id='password']"))
 )
 login_button = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit2']"))
+    EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))
 )
 
 username_input.send_keys("admin")
